d0f797e3b1e0413c07d239058b97507a8a8709e1
Sophie Ragot <ragot.s@free.fr>
2021-02-16 23:24:23 +0100

    installation webpack

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,19 @@ Encore
     .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
+   
+    .copyFiles({
+                from: './assets/images',
+       
+                 // optional target path, relative to the output dir
+                 //to: 'images/[path][name].[ext]',
+        
+                 // if versioning is enabled, add the file hash too
+                 //to: 'images/[path][name].[hash:8].[ext]',
+        
+                 // only copy files matching this pattern
+                 //pattern: /\.(png|jpg|jpeg)$/
+             })
 
     /*
      * ENTRY CONFIG
@@ -20,10 +33,9 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry('app', './assets/js/app.js')
 
-    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/controllers.json')
+  
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -32,13 +44,7 @@ Encore
     // but, you probably want this, unless you're building a single-page app
     .enableSingleRuntimeChunk()
 
-    /*
-     * FEATURE CONFIG
-     *
-     * Enable & configure other features below. For a full
-     * list of features, see:
-     * https://symfony.com/doc/current/frontend.html#adding-more-features
-     */
+    
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
@@ -56,7 +62,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
@@ -66,10 +72,10 @@ Encore
 
     // uncomment to get integrity="..." attributes on your script & link tags
     // requires WebpackEncoreBundle 1.4 or higher
-    //.enableIntegrityHashes(Encore.isProduction())
+    .enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+   .autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
