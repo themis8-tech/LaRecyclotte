e91cdd80e9a0c31fdd4f122a54eb05f5289cc553
Mounir <malfi.mounir@gmail.com>
2021-02-17 15:44:46 +0100

    creation du controller product et template list

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
+    .addEntry('page_product', './assets/js/pages/product.js')
 
   
 
