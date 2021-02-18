56d0aa84d4ccafc6a9f402d47878d4214ea0ea8d
Mounir <malfi.mounir@gmail.com>
2021-02-18 10:41:19 +0100

    modif productCtrl

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,11 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+// #[Route('/product', name: 'product_')]
 
-#[Route('/product', name: 'product_')]
+/**
+* @Route("/product", name="product_")
+*/
 class ProductController extends AbstractController
 {
-    #[Route('/list', name: 'list')]
+    // #[Route('/list', name: 'list')]
+
+    /**
+    * @Route("/list", name="list")
+    */
     public function list(): Response
     {
         return $this->render('product/list.html.twig', [
@@ -17,7 +24,11 @@ class ProductController extends AbstractController
         ]);
     }
 
-    #[Route('/display', name: 'display')]
+    // #[Route('/display', name:'display')]
+
+    /**
+    * @Route("/display", name="display")
+    */
     public function display(): Response
     {
         return $this->render('product/display.html.twig', [
