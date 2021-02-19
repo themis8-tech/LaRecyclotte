e120a1f3a9f2cd582d216eaa2d4d3e19beaf1ad4
florinedrm <florine.drm@gmail.com>
2021-02-19 20:35:58 +0100

    homepage slider js work

--- assets/js/pages/homepage.js
+++ assets/js/pages/homepage.js
@@ -1,3 +1,17 @@
 import '../../css/pages/homepage.scss';
 
-new Splide( '.splide' ).mount();
+// SLIDER
+new Splide( '.splide', {
+	perPage: 3,
+    perMove: 1,
+    breakpoints: {
+        992: {
+            perPage: 2,
+        },
+        768: {
+            perPage: 1,
+        },
+    },
+    rewind: true,
+    pagination: false,
+} ).mount();
