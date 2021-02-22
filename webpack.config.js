8f332cf2cf742aa15d28d3b9223d2de5ac25cc60
florinedrm <florine.drm@gmail.com>
2021-02-22 18:22:46 +0100

    page product display integration v1

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,7 @@ Encore
     .addEntry('page_home', './assets/js/pages/homepage.js')
     .addEntry('page_product', './assets/js/pages/product.js')
     .addEntry('page_concept', './assets/js/pages/concept.js')  
+    .addEntry('page_product-display', './assets/js/pages/product-display.js')  
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
