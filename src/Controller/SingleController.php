830591a233ee7bc2e6e6d86c7333844152730d29
A-Dial <abdoulayejoseph.diallo@gmail.com>
2021-02-23 10:05:39 +0100

    ajout SingleController

--- src/Controller/SingleController.php
+++ src/Controller/SingleController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ConceptController extends AbstractController
+class SingleController extends AbstractController
 {
     /**
      * @Route("/concept", name="concept")
