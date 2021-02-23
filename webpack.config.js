7f7e51f111e7a847277a9aab4ff49d09933a6660
florinedrm <florine.drm@gmail.com>
2021-02-23 10:26:35 +0100

    fix conflict webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,6 @@ Encore
     .addEntry('page_product', './assets/js/pages/product.js')
     .addEntry('page_concept', './assets/js/pages/concept.js')
     .addEntry('page_profile', './assets/js/pages/profile.js')  
-    .addEntry('page_concept', './assets/js/pages/concept.js')  
     .addEntry('page_product-display', './assets/js/pages/product-display.js')  
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
