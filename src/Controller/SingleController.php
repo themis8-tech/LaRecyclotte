90f5c0799d0f1ddfcc98a0dcfcf90b277819e903
Mathieu Staelen <sazm.dev@gmail.com>
2021-02-24 14:48:58 +0100

    contact

--- src/Controller/SingleController.php
+++ src/Controller/SingleController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use App\entity\Contact;
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
+
+
 
 class SingleController extends AbstractController
 {
@@ -17,4 +23,63 @@ class SingleController extends AbstractController
             'controller_name' => 'ConceptController',
         ]);
     }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+
+    public function contact(Request $request, MailerInterface $mailer)
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $contactFormData = $form->getData();
+
+            $message = (new Email())
+                ->from($contactFormData['email'])
+                ->to('@gmail.com')
+                ->subject('Mail recu')
+                ->text('Sender : '.$contactFormData['email'].\PHP_EOL.
+                    $contactFormData['Message'],
+                    'text/plain');
+            $mailer->send($message);
+
+
+
+
+            $this->addFlash('success', 'Votre message est envoyé');
+
+            return $this->redirectToRoute('contact');
+        }
+
+
+
+        return $this->render('contact/index.html.twig', [
+            'our_form' => $form->createView()
+        ]);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
