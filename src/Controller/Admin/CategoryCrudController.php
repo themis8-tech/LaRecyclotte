afc60da6671c6fb6bde0a81eef0a28d81317f0e0
Sophie Ragot <ragot.s@free.fr>
2021-02-27 10:58:32 +0100

    access restriction moderator

--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -5,9 +5,15 @@ namespace App\Controller\Admin;
 use App\Entity\Category;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+
+/**
+* @IsGranted("ROLE_ADMIN", statusCode=403, message="Accès résérvé aux adminstrateurs")
+*/
 class CategoryCrudController extends AbstractCrudController
 {
+    
     public static function getEntityFqcn(): string
     {
         return Category::class;
